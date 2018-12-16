40110ef891105befcc1b4bddef57bbbfd9b64e7b
aserfend <liuchangm7j@vip.qq.com>
2018-12-16 15:13:08 +0800

    静态化锚点计算

--- xsp/src/base.lua
+++ xsp/src/base.lua
@@ -106,44 +106,7 @@ function getScaleMainPoint(MainPoint,Anchor,Arry)	--缩放锚点
 		y=MainPoint.y-Arry.Dev.Top,
 	}
 	local x,y
-	local switch={
-		["Middle"]=function()
-			x=Arry.Cur.x/2-((Arry.Dev.x/2-point.x)*Arry.MainPointsScaleMode)+Arry.Cur.Left
-			y=Arry.Cur.y/2-((Arry.Dev.y/2-point.y)*Arry.MainPointsScaleMode)+Arry.Cur.Top
-		end,
-		["Left"]=function ()--左中
-			x=point.x*Arry.MainPointsScaleMode+Arry.Cur.Left
-			y=Arry.Cur.y/2-((Arry.Dev.y/2-point.y)*Arry.MainPointsScaleMode)+Arry.Cur.Top
-		end,
-		["Right"]=function ()--右中
-			x=Arry.Cur.x-((Arry.Dev.x-point.x)*Arry.MainPointsScaleMode)+Arry.Cur.Left
-			y=Arry.Cur.y/2-((Arry.Dev.y/2-point.y)*Arry.MainPointsScaleMode)+Arry.Cur.Top
-		end,
-		["Top"]=function ()--上中 
-			x=Arry.Cur.x/2-((Arry.Dev.x/2-point.x)*Arry.MainPointsScaleMode)+Arry.Cur.Left
-			y=MainPoint.y*Arry.MainPointsScaleMode+Arry.Cur.Top
-		end,
-		["Bottom"]=function ()--下中
-			x=Arry.Cur.x/2-((Arry.Dev.x/2-point.x)*Arry.MainPointsScaleMode)+Arry.Cur.Left
-			y=Arry.Cur.y-((Arry.Dev.y-point.y)*Arry.MainPointsScaleMode)+Arry.Cur.Top
-		end,
-		["LeftTop"]=function ()--左上
-			x=point.x*Arry.MainPointsScaleMode+Arry.Cur.Left
-			y=point.y*Arry.MainPointsScaleMode+Arry.Cur.Top
-		end,
-		["LeftBottom"]=function ()--左下
-			x=point.x*Arry.MainPointsScaleMode+Arry.Cur.Left
-			y=Arry.Cur.y-((Arry.Dev.y-point.y)*Arry.MainPointsScaleMode)+Arry.Cur.Top
-		end,
-		["RightTop"]=function () --右上角
-			x=Arry.Cur.x-((Arry.Dev.x-point.x)*Arry.MainPointsScaleMode)+Arry.Cur.Left
-			y=point.y*Arry.MainPointsScaleMode+Arry.Cur.Top
-		end,
-		["RightBottom"]=function () --右下角
-			x=Arry.Cur.x-((Arry.Dev.x-point.x)*Arry.MainPointsScaleMode)+Arry.Cur.Left
-			y=Arry.Cur.y-((Arry.Dev.y-point.y)*Arry.MainPointsScaleMode)+Arry.Cur.Top
-		end,
-	}
+	
 	switch[Anchor]()
 	return {x=x,y=y}
 end
--- xsp/src/const.lua
+++ xsp/src/const.lua
@@ -13,7 +13,48 @@ _const={
 	offsetMode="withArry",
 	GetColorMode="getBilinear",
 	Arry=nil,
-	
+	switch={
+		Middle=function()
+			x=Arry.Cur.x/2-((Arry.Dev.x/2-point.x)*Arry.MainPointsScaleMode)+Arry.Cur.Left
+			y=Arry.Cur.y/2-((Arry.Dev.y/2-point.y)*Arry.MainPointsScaleMode)+Arry.Cur.Top
+		end,
+		Left=function ()--左中
+			x=point.x*Arry.MainPointsScaleMode+Arry.Cur.Left
+			y=Arry.Cur.y/2-((Arry.Dev.y/2-point.y)*Arry.MainPointsScaleMode)+Arry.Cur.Top
+		end,
+		Right=function ()--右中
+			x=Arry.Cur.x-((Arry.Dev.x-point.x)*Arry.MainPointsScaleMode)+Arry.Cur.Left
+			y=Arry.Cur.y/2-((Arry.Dev.y/2-point.y)*Arry.MainPointsScaleMode)+Arry.Cur.Top
+		end,
+		Top=function ()--上中 
+			x=Arry.Cur.x/2-((Arry.Dev.x/2-point.x)*Arry.MainPointsScaleMode)+Arry.Cur.Left
+			y=MainPoint.y*Arry.MainPointsScaleMode+Arry.Cur.Top
+		end,
+		Bottom=function ()--下中
+			x=Arry.Cur.x/2-((Arry.Dev.x/2-point.x)*Arry.MainPointsScaleMode)+Arry.Cur.Left
+			y=Arry.Cur.y-((Arry.Dev.y-point.y)*Arry.MainPointsScaleMode)+Arry.Cur.Top
+		end,
+		LeftTop=function ()--左上
+			x=point.x*Arry.MainPointsScaleMode+Arry.Cur.Left
+			y=point.y*Arry.MainPointsScaleMode+Arry.Cur.Top
+		end,
+		TopLeft=LeftTop,
+		LeftBottom=function ()--左下
+			x=point.x*Arry.MainPointsScaleMode+Arry.Cur.Left
+			y=Arry.Cur.y-((Arry.Dev.y-point.y)*Arry.MainPointsScaleMode)+Arry.Cur.Top
+		end,
+		BottomLeft=LeftBottom,
+		RightTop=function () --右上角
+			x=Arry.Cur.x-((Arry.Dev.x-point.x)*Arry.MainPointsScaleMode)+Arry.Cur.Left
+			y=point.y*Arry.MainPointsScaleMode+Arry.Cur.Top
+		end,
+		TopRight=RightTop,
+		RightBottom=function () --右下角
+			x=Arry.Cur.x-((Arry.Dev.x-point.x)*Arry.MainPointsScaleMode)+Arry.Cur.Left
+			y=Arry.Cur.y-((Arry.Dev.y-point.y)*Arry.MainPointsScaleMode)+Arry.Cur.Top
+		end,
+		BottomRight=RightBottom
+	}
 }
 
 return _const
